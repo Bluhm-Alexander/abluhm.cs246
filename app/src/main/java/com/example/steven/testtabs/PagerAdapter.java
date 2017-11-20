4a53868ff929c3ee9b91a8388044c219d7ceea34
= <cmooref17@gmail.com>
2017-11-20 16:23:53 -0700

    Added some useful javadocs to PagerAdapter and Playlists

--- app/src/main/java/com/example/steven/testtabs/PagerAdapter.java
+++ app/src/main/java/com/example/steven/testtabs/PagerAdapter.java
@@ -19,12 +19,24 @@ public class PagerAdapter extends FragmentPagerAdapter {
         super(fm);
     }
 
+    /**
+     * Adds new fragment for a new tab with passed playlist
+     *
+     * @param fragment SongListFragment containing a listview of playlist
+     * @param playlist Playlist of songs
+     * @return returns true if NOT adding a fragment with a duplicate name of a previous one
+     */
     boolean addFragment(SongListFragment fragment, SimplePlaylist playlist) {
         Log.d(TAG, "Adding new SongListFragment with name: " + playlist.getNameOfPlaylist() + " and size of: " + playlist.size());
         if(fragmentList.contains(fragment)) {
             Log.e(TAG, "Attempted to add a new songListFragment that matches an already existing fragment");
             return false;
         }
+
+        if(playlist.size() == 0)
+            Log.w(TAG, "SimplePlaylist is empty!");
+        if(playlist == null)
+            Log.w(TAG, "SimplePlaylist is null!");
 
         fragmentList.add(fragment);
         fragmentTitleList.add(playlist.getNameOfPlaylist());
@@ -37,6 +49,14 @@ public class PagerAdapter extends FragmentPagerAdapter {
         return true;
     }
 
+    /**
+     * Adds new fragment for a new tab with passed playlist
+     *
+     * @param fragment ExpandedPlaylistFragment containing an expanded listview of playlists
+     * @param playlists Collection of playlists for ExpandedListView
+     * @return returns true if NOT adding a fragment with a duplicate name of a previous one
+     */
+
     boolean addFragment(ExpandablePlaylistFragment fragment, CompoundPlaylist playlists) {
         Log.d(TAG, "Adding ExpandablePlaylistFragment with name: " + playlists.getNameOfPlaylist() + " and size of: " + playlists.size());
         if(fragmentList.contains(fragment)) {
@@ -44,9 +64,9 @@ public class PagerAdapter extends FragmentPagerAdapter {
             return false;
         }
         if(playlists.size() == 0)
-            Log.e(TAG, "Playlist is empty!");
+            Log.w(TAG, "CompoundPlaylist is empty!");
         if(playlists == null)
-            Log.e(TAG, "Playlist is null!");
+            Log.w(TAG, "CompoundPlaylist is null!");
 
         fragmentList.add(fragment);
         fragmentTitleList.add(playlists.getNameOfPlaylist());
@@ -57,15 +77,6 @@ public class PagerAdapter extends FragmentPagerAdapter {
             moveSettingsTabToBack();
         }
         return true;
-    }
-
-
-    //Create settings tab
-    void setupSettingsTab() {
-        settingsFragment = new SettingsFragment();
-        fragmentList.add(settingsFragment);
-        fragmentTitleList.add("Settings");
-        settingsIndex = fragmentList.size() - 1;
     }
 
     //Sets the settings tab as the back tab (in case tabs get added after settings)
@@ -84,16 +95,32 @@ public class PagerAdapter extends FragmentPagerAdapter {
         fragmentList.add(settingsIndex, tmp);
     }
 
+    /**
+     * Gets name of fragment at position
+     *
+     * @param position index of fragment/tab
+     * @return returns name of fragment (name of held playlist)
+     */
     @Override
     public CharSequence getPageTitle(int position) {
         return fragmentTitleList.get(position);
     }
 
+    /**
+     * Gets fragment object at position
+     *
+     * @param position index of fragment/tab
+     * @return returns fragment object at position
+     */
     @Override
     public Fragment getItem(int position) {
         return fragmentList.get(position);
     }
 
+    /**
+     *
+     * @return returns size of fragmentList
+     */
     @Override
     public int getCount() {
         return fragmentList.size();
--- app/src/main/java/com/example/steven/testtabs/Playlist.java
+++ app/src/main/java/com/example/steven/testtabs/Playlist.java
@@ -11,80 +11,99 @@ import javax.xml.transform.Transformer;
 import javax.xml.transform.TransformerConfigurationException;
 
 /**
- * Created by xflip on 11/14/2017.
+ * SimplePlaylist
+ *
+ * Has a name
+ * Holds an array of songs
+ * Holds an index of its position in the Main Playlist Collection
  */
-
-
-class Playlist<T> extends ArrayList<T> {
-
-
-    public Playlist() {
-
-    }
-}
-
-//ArrayList of songs
 class SimplePlaylist extends ArrayList<Song> {
     private static final String TAG = "SimplePlaylist";
-    private ArrayList<Song> defaultOrder;
     private String playlistName;
     private final int indexInCollection;
 
+    /**
+     * Non-default constructor
+     *
+     * @param nameOfPlaylist Specified name of playlist
+     */
     SimplePlaylist(String nameOfPlaylist) {
         playlistName = nameOfPlaylist;
-        defaultOrder = new ArrayList<>();
         indexInCollection = -1;
         Log.w(TAG, "Created simple playlist named: " + nameOfPlaylist + " with no initialized index. Setting to -1");
     }
 
-    SimplePlaylist(String name, int indexInCollection) {
-        playlistName = name;
-        defaultOrder = new ArrayList<>();
+    /**
+     * Default constructor
+     *
+     * @param nameOfPlaylist Specified name of playlist
+     * @param indexInCollection Holds in index of its position in Main Playlist Collection
+     */
+    SimplePlaylist(String nameOfPlaylist, int indexInCollection) {
+        playlistName = nameOfPlaylist;
         this.indexInCollection = indexInCollection;
     }
 
+    /**
+     * @return Returns name of playlist
+     */
     String getNameOfPlaylist() {
         return playlistName;
     }
 
+    /**
+     * @return Returns index of its position in Main Playlist Collection
+     */
     int getIndexInCollection() {
         return indexInCollection;
     }
 }
 
-//ArrayList of playlists
+/**
+ * CompoundPlaylist
+ *
+ * Has a name
+ * Holds an array of SimplePlaylists
+ */
 class CompoundPlaylist extends ArrayList<SimplePlaylist> {
     private static final String TAG = "CompoundPlaylist";
     private String nameOfPlaylistCollection;
     private final int indexInCollection;
-    private boolean finishedAddingDefaultPlaylists;
     private int sizeOfDefaultPlaylists;
 
+    /**
+     * Default constructor
+     *
+     * @param name Name of Playlist
+     */
     CompoundPlaylist(String name) {
         Log.d(TAG, "Creating object with name: " + name);
         nameOfPlaylistCollection = name;
         indexInCollection = -1;
     }
 
+    /**
+     * Non-default constructor
+     *
+     * @param name Name of Playlist
+     * @param indexInCollection Index of its position in Main Playlist Collection
+     */
     public CompoundPlaylist(String name, int indexInCollection) {
         Log.d(TAG, "Creating object with name: " + name);
         nameOfPlaylistCollection = name;
         this.indexInCollection = indexInCollection;
     }
 
+    /**
+     * @return Returns name of playlist
+     */
     String getNameOfPlaylist() {
         return nameOfPlaylistCollection;
     }
 
-    public void setFinishedAddingDefaultPlaylists(boolean option) {
-        finishedAddingDefaultPlaylists = option;
-        sizeOfDefaultPlaylists = size();
-    }
-
-    public int sizeOfDefaultPlaylists() {
-        return sizeOfDefaultPlaylists;
-    }
-
+    /**
+     * @return Returns index of its position in Main Playlist Collection
+     */
     public int getIndexInCollection() {
         return indexInCollection;
     }
