e57f1a33949fb5d3bc7ea9efd0431b7baaac48d1
Bluhm-Alexander <31932943+Bluhm-Alexander@users.noreply.github.com>
2017-12-08 17:33:06 -0700

    Uploaded Changes to SearchFragment

--- app/src/main/java/com/example/steven/testtabs/SearchFragment.java
+++ app/src/main/java/com/example/steven/testtabs/SearchFragment.java
@@ -8,6 +8,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Adapter;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListAdapter;
 import android.widget.ListView;
@@ -20,9 +21,10 @@ import java.util.ArrayList;
  * Created by xflip on 11/14/2017.
  */
 
-public class SearchFragment extends ListFragment {
+public class SearchFragment extends Fragment {
     private static final String TAG = "SearchFragment";
     private static SimplePlaylist searchResults;
+    ListView results;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle saveInstanceState) {
@@ -30,9 +32,16 @@ public class SearchFragment extends ListFragment {
         ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.search_layout, container, false);
 
         searchResults = AppCore.getInstance().mediaStorage.createSimplePlaylist("searchResults");
+        results = (ListView) rootView.findViewById(R.id.results);
+        results.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Toast.makeText(getContext(), "test", Toast.LENGTH_SHORT);
+            }
+        });
 
         SongAdapter adapter = new SongAdapter(getActivity(), searchResults);
-        setListAdapter(adapter);
+        results.setAdapter(adapter);
 
         setRetainInstance(true);
 
@@ -42,20 +51,6 @@ public class SearchFragment extends ListFragment {
 
     private static void clearResults() {
         searchResults.clear();
-
     }
 
-
-
-    @Override
-    public void onListItemClick(ListView l, View v, int position, long id) {
-        super.onListItemClick(l, v, position, id);
-
-        switch(position) {
-            case 0:
-                //Nothing for now
-                Toast.makeText(getActivity(), "Album art: Off", Toast.LENGTH_SHORT).show();
-                break;
-        }
-    }
 }
